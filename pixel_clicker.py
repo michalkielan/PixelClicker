07216a12c845bbd01901184a36059a03c47526af
Michal Kielan <michalkielan@protonmail.com>
2019-01-18 06:14:36 +0100

    remove one character variables, lint

--- pixel_clicker.py
+++ pixel_clicker.py
@@ -1,40 +1,46 @@
 #!/usr/bin/env python3.5
-import cv2
-import sys
+"""Script to read the rgb color of each pixel"""
 
+import sys
+import cv2
 
 class ImageProcessing:
+  """ImageProcessing class for image processing """
+
   def __init__(self, filename):
     self.filename = filename
     self.img = cv2.imread(self.filename)
 
-  def __read_colors__(self, x, y):
-    color = self.img[y, x, :]
-    b, g, r = color
-    print(r, '\t', g, '\t', b)
+  def __read_colors__(self, pos):
+    pos_x, pos_y = pos
+    color = self.img[pos_y, pos_x, :]
+    val_b, val_g, val_r = color
+    print(val_r, '\t', val_g, '\t', val_b)
 
-  def __mouse_event_processing__(self, x, y):
-    self.__read_colors__(x, y)
+  def __mouse_event_processing__(self, pos):
+    self.__read_colors__(pos)
 
-  def on_mouse_event(self, event, x, y, flags, param):
+  def on_mouse_event(self, event, pos_x, pos_y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
-      self.__mouse_event_processing__(x, y)
+      self.__mouse_event_processing__((pos_x, pos_y))
 
   def processing(self):
     cv2.imshow(self.filename, self.img)
 
-    cv2.set_mouse_callback(self.filename, self.on_mouse_event)
+    cv2.setMouseCallback(self.filename, self.on_mouse_event)
     pressedkey = cv2.waitKey(0)
 
     if pressedkey == 27:
       cv2.destroyAllWindows()
 
-
-if __name__ == '__main__':
+def main():
   if len(sys.argv) < 2:
     print('Specify image name: ./PixelClicker.py image_file.jpg')
   else:
-    filename = sys.argv[1]
-    print('Filename: ', filename)
-    img = ImageProcessing(filename)
+    img_filename = sys.argv[1]
+    print('Filename: ', img_filename)
+    img = ImageProcessing(img_filename)
     img.processing()
+
+if __name__ == '__main__':
+  main()

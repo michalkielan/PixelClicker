b7248bbe674d099720e6e94c8f43ec73f44b8cb4
Michal Kielan <michalkielan@protonmail.com>
2019-01-18 07:07:38 +0100

    print rectangle first try

--- PixelClicker.py
+++ PixelClicker.py
@@ -2,6 +2,9 @@
 import cv2
 import sys
 
+drawing=False # true if mouse is pressed
+mode=True # if True, draw rectangle. Press 'm' to toggle to curve
+
 
 class ImageProcessing:
   def __init__(self, filename):
@@ -16,19 +19,46 @@ class ImageProcessing:
     self.__readColors__(x, y)
 
   def onMouseEvent(self, event, x, y, flags, param):
-    if event == cv2.EVENT_LBUTTONDOWN:
-       self.__mouseEventProcessing__(x, y)
+  #  if event == cv2.EVENT_LBUTTONDOWN:
+  #     self.__mouseEventProcessing__(x, y)
+    global ix,iy,drawing, mode
+
+    if event==cv2.EVENT_LBUTTONDOWN:
+      drawing=True
+      ix,iy=x,y
+
+    elif event==cv2.EVENT_MOUSEMOVE:
+      if drawing==True:
+        if mode==True:
+          cv2.rectangle(self.img,(ix,iy),(x,y),(0,0,255),10)
+     #     cv2.imshow(self.filename, self.img)
+
+    elif event==cv2.EVENT_LBUTTONUP:
+      drawing=False
+      if mode==True:
+        cv2.rectangle(self.img,(ix,iy),(x,y),(0,0,255),10)
+    #    cv2.imshow(self.filename, self.img)
 
   def processing(self):
     self.img = cv2.imread(self.filename)
     cv2.imshow(self.filename, self.img)
 
     cv2.setMouseCallback(self.filename, self.onMouseEvent)
-    self.pressedkey = cv2.waitKey(0)
 
-    if self.pressedkey == 27:
-        cv2.destroyAllWindows()
+    while(1):
+      cv2.imshow(self.filename, self.img)
+      k = cv2.waitKey(1)&0xFF
+      if k == 27:
+        break
+      cv2.destroyAllWindows()
 
+#    self.pressedkey = cv2.waitKey(0)
+
+#    while True:
+#      cv2.imshow(self.filename, self.img)
+#    
+#    if self.pressedkey == 27:
+#      cv2.destroyAllWindows()
 
 if __name__ =='__main__':
   if len(sys.argv) < 2:

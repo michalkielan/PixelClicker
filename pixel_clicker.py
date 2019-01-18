ed3604dc1d3b037950262e2df0ffe79f2eb86e58
Michal Kielan <michalkielan@protonmail.com>
2019-01-18 05:42:46 +0100

    fix: indentation, spaces, init values outside __init__

--- pixel_clicker.py
+++ pixel_clicker.py
@@ -6,31 +6,31 @@ import sys
 class ImageProcessing:
   def __init__(self, filename):
     self.filename = filename
+    self.img = cv2.imread(self.filename)
 
-  def __readColors__(self, x, y):
+  def __read_colors__(self, x, y):
     color = self.img[y, x, :]
     b, g, r = color
     print(r, '\t', g, '\t', b)
 
-  def __mouseEventProcessing__(self, x, y):
-    self.__readColors__(x, y)
+  def __mouse_event_processing__(self, x, y):
+    self.__read_colors__(x, y)
 
-  def onMouseEvent(self, event, x, y, flags, param):
+  def on_mouse_event(self, event, x, y, flags, param):
     if event == cv2.EVENT_LBUTTONDOWN:
-       self.__mouseEventProcessing__(x, y)
+      self.__mouse_event_processing__(x, y)
 
   def processing(self):
-    self.img = cv2.imread(self.filename)
     cv2.imshow(self.filename, self.img)
 
-    cv2.setMouseCallback(self.filename, self.onMouseEvent)
-    self.pressedkey = cv2.waitKey(0)
+    cv2.set_mouse_callback(self.filename, self.on_mouse_event)
+    pressedkey = cv2.waitKey(0)
 
-    if self.pressedkey == 27:
-        cv2.destroyAllWindows()
+    if pressedkey == 27:
+      cv2.destroyAllWindows()
 
 
-if __name__ =='__main__':
+if __name__ == '__main__':
   if len(sys.argv) < 2:
     print('Specify image name: ./PixelClicker.py image_file.jpg')
   else:

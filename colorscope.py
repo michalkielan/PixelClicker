63c39e2b34ced28d2f578b23799263e4137322e9
Michal Kielan <michalkielan@protonmail.com>
2019-01-21 21:35:49 +0100

    get rect roi from every corner: works

--- colorscope.py
+++ colorscope.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3.5
-"""Script to read the color data of each pixel"""
+"""Script to read the color values"""
 
 import abc
 import argparse
 import sys
 import os
 import cv2
+import numpy as np
 
 
 class MouseRectDrawer():
@@ -35,20 +36,42 @@ class MouseRectDrawer():
 
 class ColorReader(metaclass = abc.ABCMeta):
   def __init__(self, filename):
+    rect_color = (0, 0, 255)
     self.__filename = filename
     self.__window = self.__filename
     self._img = cv2.imread(self.__filename)
     self._img_mark = self._img.copy()
-    self.__mouse_drawer = MouseRectDrawer(self.__window, self._img, (0, 0, 255))
+    self.__mouse_drawer = MouseRectDrawer(self.__window, self._img, rect_color)
     self.__rect = [[0, 0], [0, 0]]
 
   @abc.abstractmethod
-  def _read_colors(self, pos):
+  def _read_pixel_color(self, pos):
     pass
+
+  def __read_rect_color(self, rect):
+    p1_x = rect[0][0]
+    p1_y = rect[1][0]
+    p2_x = rect[0][1]
+    p2_y = rect[1][1]
+
+    print(rect)
+    
+
+    min_x = min(p1_x, p2_x)
+    min_y = min(p1_y, p2_y)
+    max_x = max(p1_x, p2_x)
+    max_y = max(p1_y, p2_y)
+    
+    print(p2_x, '\t', p2_y, '\t', p1_x, '\t', p1_y)
+    print(min(p2_x, p2_y), '\t', max(p2_x, p2_y), '\t', min(p1_x, p1_y), '\t', max(p1_x, p1_y))
+
+    roi = self._img[min(p2_x, p2_y):max(p2_x, p2_y), min(p1_x, p1_y):max(p1_x, p1_y)]
+#    roi = self._img[p2_x:p2_y, p1_x:p1_y]
+    cv2.imshow('window2', roi)
 
   def __filer(self, rect):
     # to be implemented: average, median etc.
-    return [255, 0 255]
+    return [255, 0, 255]
 
   def __mouse_event_processing(self, pos):
     color = self._read_colors(pos)
@@ -66,8 +89,7 @@ class ColorReader(metaclass = abc.ABCMeta):
     elif event == cv2.EVENT_LBUTTONUP:
       self.__mouse_drawer.mouse_up_event((x, y))
       self.__rect[1] = [x, y]
-      color = self.__filter(rect)
-      print(
+      self.__read_rect_color(self.__rect)
 
   def processing(self):
     cv2.imshow(self.__window, self._img)
@@ -87,7 +109,7 @@ class ColorReaderRGB(ColorReader):
     super().__init__(filename)
     print('R\tG\tB')
 
-  def _read_colors(self, pos):
+  def _read_pixel_color(self, pos):
     super()._read_colors(pos)
     pos_x, pos_y = pos
     b, g, r = self._img[pos_y, pos_x, :]
@@ -99,7 +121,7 @@ class ColorReaderYUV(ColorReader):
     super().__init__(filename)
     print('Y\tU\tV')
 
-  def _read_colors(self, pos):
+  def _read_pixel_color(self, pos):
     super()._read_colors(pos)
     pos_x, pos_y = pos
     img_yuv = cv2.cvtColor(self._img, cv2.COLOR_BGR2YUV)

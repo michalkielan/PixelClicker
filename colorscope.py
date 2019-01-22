14de6bb33844191161f1f7d370db66215f15ebe8
Michal Kielan <michalkielan@protonmail.com>
2019-01-22 01:33:46 +0100

    move filtering outside to class

--- colorscope.py
+++ colorscope.py
@@ -5,11 +5,38 @@ import abc
 import argparse
 import sys
 import os
-import statistics
+import numpy
 import cv2
 
 
-class MouseRectDrawer():
+class ColorChannelFilter:
+  def __init__(self, img):
+    self.__channel_data = self.__get_channel_data(img)
+
+  def __get_channel_data(self, img):
+    h, w, num_channels = img.shape
+    channel_data = [[] for i in range(num_channels)]
+    for y in range(0, h):
+      for x in range(0, w):
+        channels = img[y, x, :]
+        for i in range(0, num_channels):
+          channel_data[i].append(int(channels[i]))
+    return channel_data
+
+  def median(self):
+    channel_filtered = []
+    for channel_val in self.__channel_data:
+      channel_filtered.append(int(numpy.median(channel_val)))
+    return channel_filtered
+
+  def average(self):
+    channel_filtered = []
+    for channel_val in self.__channel_data:
+      channel_filtered.append(int(numpy.average(channel_val)))
+    return channel_filtered
+
+
+class MouseRectDrawer:
   def __init__(self, window, image, color):
     self.__is_drawing = False
     self.__start_pos = [0, 0]
@@ -34,7 +61,7 @@ class MouseRectDrawer():
     cv2.imshow(self.__window, self.__img)
 
 
-class ColorReader(metaclass = abc.ABCMeta):
+class ColorReader(metaclass=abc.ABCMeta):
   def __init__(self, filename):
     rect_color = (0, 0, 255)
     self.__filename = filename
@@ -48,32 +75,17 @@ class ColorReader(metaclass = abc.ABCMeta):
   def _get_color_format(self, img_roi):
     pass
 
-  def __filter(self, channel_data):
-    channel_val = []
-    for ch in channel_data:
-      channel_val.append(int(statistics.median(ch)))
-    return channel_val
-
-  def __calc_roi_color(self, img_roi):
-    h, w, num_channels = img_roi.shape
-    channel_data = [[] for i in range(num_channels)]
-    for y in range(0, h):
-      for x in range(0, w):
-        channels = img_roi[y, x, :]
-        for i in range(0, num_channels):
-          channel_data[i].append(int(channels[i]))
-
-    return self.__filter(channel_data)
-  
   def __read_rect_color(self, rect):
     p1_x, p1_y = [rect[0][0], rect[0][1]]
     p2_x, p2_y = [rect[1][0], rect[1][1]]
 
-    min_x, min_y = [min(p1_x, p2_x), min(p1_y, p2_y)] 
-    max_x, max_y = [max(p1_x, p2_x), max(p1_y, p2_y)] 
+    min_x, min_y = [min(p1_x, p2_x), min(p1_y, p2_y)]
+    max_x, max_y = [max(p1_x, p2_x), max(p1_y, p2_y)]
 
     roi = self._img[min_y:max_y, min_x:max_x]
-    return self.__calc_roi_color(self._get_color_format(roi))
+
+    color_filter = ColorChannelFilter(self._get_color_format(roi))
+    return color_filter.median()
 
   def __on_mouse_event(self, event, x, y, flags, param):
     del flags, param
@@ -87,10 +99,10 @@ class ColorReader(metaclass = abc.ABCMeta):
     elif event == cv2.EVENT_LBUTTONUP:
       self.__mouse_drawer.mouse_up_event((x, y))
       self.__rect[1] = [x, y]
-  
+
       color = self.__read_rect_color(self.__rect)
       print(color[0], '\t', color[1], '\t', color[2])
-      
+
 
   def processing(self):
     cv2.imshow(self.__window, self._img)

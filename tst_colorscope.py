a9427d6d5092a54f4914d3ce570bc28bab6aceab
Michal Kielan <michalkielan@protonmail.com>
2019-01-24 11:52:36 -0800

    update test: add missing img loader

--- tst_colorscope.py
+++ tst_colorscope.py
@@ -121,7 +121,7 @@ class TestColorscope(unittest.TestCase):
 
   def test_image_loader_nv21(self):
     if not is_windows():
-      imaloader = colorscope.ImageLoaderRawNV12(self.res.raw_nv12_1920_1080, [1920, 1080])
+      imloader = colorscope.ImageLoaderRawNV12(self.res.raw_nv12_1920_1080, [1920, 1080])
       img = imloader.imread()
       h, w, channels = img.shape
       self.assertEqual([1080, 1920], [h, w])

55b46b4bb7250ced37c3488c5d7ed1c83562cf7f
Michal Kielan <michalkielan@protonmail.com>
2019-01-24 11:13:13 -0800

    test: fix typo

--- tst_colorscope.py
+++ tst_colorscope.py
@@ -120,7 +120,7 @@ class TestColorscope(unittest.TestCase):
 
   def test_image_loader_nv21(self):
     if not is_windows():
-      image_loader = colorscope.ImageLoaderRawNV21(self.res.raw_nv12_1920_1080, size_1080p)
+      image_loader = colorscope.ImageLoaderRawNV24(self.res.raw_nv12_1920_1080, (1920, 1080)
       img = image_loader.imread()
       h, w, channels = img.shape
       self.assertEqual([1080, 1920], [h, w])
